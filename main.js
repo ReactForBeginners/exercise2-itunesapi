aba4ef75282f4e045a7802fd3e472a5882cd0aa3
perborgen <perhb@hotmail.com>
2015-08-10 14:21:11 +0200

    readme

--- main.js
+++ main.js
@@ -1,89 +1,17 @@
-var SearchContainer = React.createClass({
+'use strict';
+
+var RESULTS = ["James Bond", "Superman", "Jason Bourne", "Terminator"];
+
+var SearchApp = React.createClass({
 	render: function(){
 		return (
 			<div>
-				<input type="text" ref="query" />
-				<select ref="category">
-					<option value="software">Apps</option>
-					<option value="movie">Films</option>
-					<option value="music">Music</option>
-				</select>
-				<input type="submit" onClick={this.createAjax} />
+			{this.props.results}
 			</div>
 		);
-	},
-
-	createAjax: function(){
-		var query 	 	= React.findDOMNode(this.refs.query).value;
-		var category 	= React.findDOMNode(this.refs.category).value;
-		var URL = 'https://itunes.apple.com/search?term=' + query +'&country=us&entity=' + category;
-		this.props.search(URL)
 	}
 });
 
-var ResultContainer = React.createClass({
-	render: function(){
-		var apps = this.props.searchResult.map(function(app,index){
-			return <li key={index}>{app.trackName}</li>
-		})
-		return(
-			<ul>
-				{apps}
-			</ul>			
-		);
-	}
-});
-
-/*var Item = React.createClass({
-	render: function(){
-		return(
-
-		);
-	}
-});
-*/
-
-var App = React.createClass({
-	
-	getInitialState: function(){
-		return {
-			searchResult: []
-		}
-	},
-
-	render: function(){
-		return (
-			<div>
-			<SearchContainer 
-				search={this.search} />
-			<ResultContainer 
-				searchResult={this.state.searchResult} />
-
-			</div>
-		);
-	},
-
-	showApps: function(response){
-		console.log(response.results);
-		this.setState({
-			searchResult: response.results
-		})
-	},
-
-	search: function(URL){
-		$.ajax({
-			type: "GET",
-			dataType: 'jsonp',
-			url: URL,
-			success: function(response){
-				this.showApps(response);
-			}.bind(this)
-		});
-	}
 
 
-});
-
-
-
-React.render(<App />,  document.getElementById("content"));
+React.render(<SearchApp results={RESULTS}/>,  document.getElementById("content"));
